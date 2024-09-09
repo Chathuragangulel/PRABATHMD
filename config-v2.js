2ce0e43d187e938056ac4c2f78b8efff0a2478a0
Chathuragangulel <chathuragangulel@gmail.com>
2024-09-09 15:22:10 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~SPInHKSb#FxbXTcme-ndYy1HFUwO9zQAZst3EK1VAax-Nh13B-K4"
 
 
 module.exports = {
